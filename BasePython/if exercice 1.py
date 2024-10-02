3de7630e44c96ecbd6c70f6479063aadcff018ba
winandISA <michael.winand@isagosselies.be>
2024-10-02 07:32:53 +0200

    first commit

---
+++ BasePython/if exercice 1.py
@@ -0,0 +1,9 @@
+nombre1 = int(input("Entrez le nombre 1: "))
+nombre2 = int(input("Entrez le nombre 2: "))
+
+if nombre1 == nombre2:
+    print("Les deux nombres sont égaux")
+elif nombre1 > nombre2:
+    print("le nombre 1 est plus grand")
+else :
+    print("le nombre 2 est plus grand")
---
+++ BasePython/if exercice 2.py
@@ -0,0 +1,7 @@
+nombre1 = int(input("Entrez le nombre 1: "))
+nombre2 = int(input("Entrez le nombre 2: "))
+nombre3 = int(input("Entrez le nombre 3: "))
+
+resultat = (nombre1 + nombre2 + nombre3) / 3
+
+print(f"la moyenne est {resultat}.")

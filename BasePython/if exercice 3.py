dd97b772e2a81a08693b2b95d13bd2a2a2f6ab52
mikross <mikross20@hotmail.com>
2024-10-02 07:34:41 +0200

    ex3

---
+++ BasePython/if exercice 3.py
@@ -0,0 +1,14 @@
+import random
+
+na = random.randint(0, 10)
+
+continuer = True
+while(continuer):
+    nu = int(input("Faites un essai: "))
+    if nu == na:
+        print("vous avez gagné")
+    else:
+        if nu > na:
+            print("le chiffre est plus petit")
+        else:
+            print("le chiffre est plus grand")
